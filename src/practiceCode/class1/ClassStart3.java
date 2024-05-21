06ae4ae1db6cad039f1c5816f3ff670b98bb1fcb
seeyoungryu <seeyoungryu@gmail.com>
2024-05-21 19:09:22 +0900

    update - class

--- src/practiceCode/class1/ClassStart3.java
+++ src/practiceCode/class1/ClassStart3.java
@@ -3,13 +3,13 @@ package practiceCode.class1;
 public class ClassStart3 {
 
     public static void main(String[] args) {
-        Student student1;   //학생을 담을 변순 선언
-        student1 = new Student();
+        Student student1;   //학생을 담을 변수 선언
+        student1 = new Student();      // Student 클래스를 사용해서 실제 메모리에 인스턴스(객체 -> studnet1 )를 만듦
         student1.name = "학생1";
         student1.age = 15;
         student1.grade = 90;
 
-        Student student2 = new Student();
+        Student student2 = new Student(); // Student 클래스를 사용해서 실제 메모리에 인스턴스(객체 -> studnet2 )를 만듦
         student2.name = "학생2";
         student2.age = 18;
         student2.grade = 80;
