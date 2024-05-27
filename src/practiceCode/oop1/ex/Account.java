2f3c7582f9c14ceaaa54c35b920b14e8a9f77531
seeyoungryu <seeyoungryu@gmail.com>
2024-05-28 08:44:06 +0900

    update - package oop1.ex

--- src/practiceCode/oop1/ex/Account.java
+++ src/practiceCode/oop1/ex/Account.java
@@ -3,11 +3,11 @@ package practiceCode.oop1.ex;
 public class Account {
     int balance; //잔액
 
-    void deposit(int amount) {
+    void deposit(int amount) { //입금
         balance += amount;
     }
 
-    void withdraw(int amount) {
+    void withdraw(int amount) { //출금
         if (balance >= amount) {
             balance -= amount;
         } else {
