3cb257d3ad1628abb41248af95648e15542c1286
seeyoungryu <seeyoungryu@gmail.com>
2024-05-28 09:13:12 +0900

    update - package construct

--- src/practiceCode/construct/MemberThis.java
+++ src/practiceCode/construct/MemberThis.java
@@ -4,6 +4,7 @@ public class MemberThis {
     String nameField;
 
     void initMember(String nameParameter) {
-        nameField = nameParameter;
+        this.nameField = nameParameter;
+        //현재 매개변수에 해당 변수의 이름 x 이므로 자동으로 멤버변수로 매칭됨 -> nameField = nameParameter; 라고만 써도 무방한 상태임 지금은
     }
 }
