fddcc2b543b9a60c798f1e03e7fe8e8771ff03e8
seeyoungryu <seeyoungryu@gmail.com>
2024-05-30 08:06:27 +0900

    add package final1

--- src/practiceCode/final1/FinalRefMain.java
+++ src/practiceCode/final1/FinalRefMain.java
@@ -6,9 +6,30 @@ public class FinalRefMain {
         //data = new Data();
 
         //참조 대상의 값은 변경 가능
+
+        System.out.println(data);
         data.value = 10;
+        System.out.println(data);
         System.out.println(data.value);
+        System.out.println(data);
+
         data.value = 20;
+        System.out.println(data);
+        System.out.println(data.value);
+        System.out.println(data);
         System.out.println(data.value);
     }
 }
+
+
+/*practiceCode.final1.Data@2f92e0f4
+practiceCode.final1.Data@2f92e0f4
+10
+practiceCode.final1.Data@2f92e0f4
+practiceCode.final1.Data@2f92e0f4
+20
+practiceCode.final1.Data@2f92e0f4
+20
+*/
+
+//참조 대상만 변경 불가 (주소값=참조값 동일)
