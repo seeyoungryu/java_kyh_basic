56c762477987a4359da0574f7b42fa91fb1ba1f3
seeyoungryu <seeyoungryu@gmail.com>
2024-05-29 19:25:30 +0900

    update package static1

--- src/practiceCode/static1/Data3.java
+++ src/practiceCode/static1/Data3.java
@@ -1,7 +1,7 @@
 package practiceCode.static1;
 
 public class Data3 {
-    public static int count; //static
+    public static int count; //static --> 메서드 영역에서 관리되는.. static 변수를 사용해서 공용 변수를 관리
     public String name;
 
     public Data3(String name) {
